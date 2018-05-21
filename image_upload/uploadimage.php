f95dddabac1710111acce71b6e4ff05a4e7871fc
Louise Jean <louisejean8102@gmail.com>
2018-05-21 17:41:05 +0100

    image_upload_sys please checkout readme file first for explaination

---
+++ image_upload/uploadimage.php
@@ -0,0 +1,16 @@
+	
+<!DOCUTYPE html>
+<html>
+    <head>
+        <title>
+            upload file test page
+        </title>
+    </head>
+        <div>
+            <form action="upload.php" method="POST" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <button type="submit" name="submit">UPLOAD</button>
+            </form>
+        </div>
+    
+    </html>
