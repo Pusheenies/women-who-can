0efc408adfb2dd17a380c6d343055dd056ef7730
Laura <35954013+LauraCollard@users.noreply.github.com>
2018-05-22 10:01:58 +0100

    MVC profile page, no pic yet

--- profileModel.php
+++ profileModel.php
@@ -3,20 +3,30 @@ session_start();
 include "pdo.php";
 include "class_lib.php";
 
-$_SESSION["id"]= 2;
+$_SESSION["id"]= 15;
 
 //fetching member details
 $stmt= $pdo->prepare("SELECT * FROM members WHERE member_id=:id");
 $stmt->execute(array(":id" => $_SESSION["id"]));
 $member= $stmt->fetch(PDO::FETCH_ASSOC);
-$member_object= new Member($member["member_id"], $member["username"], $member["password"],
-$member["security_group"], $member["registration_date"]);
+$member_object= new Member($member["member_id"], htmlentities($member["username"]), htmlentities($member["password"]),
+                            $member["security_group"], $member["registration_date"]);
+
+//if writer, fetching posts that member has written
+if($member["security_group"]==="writer"){
+    $own_posts= $member_object->getOwnPosts($pdo);
+    $member["own_posts"]= [];
+    foreach($own_posts as $post){
+        $post_details= [htmlentities($post->getTitle()), $post->getPost_id(), $post->getPost_date()];
+        array_push($member["own_posts"], $post_details);
+    }
+}
 
 //fetching member favourite posts
 $favourites= $member_object->getFavourites($pdo);
 $member["favourites"]= [];
 foreach($favourites as $favourite){
-    $favourite_details= [$favourite->getTitle(), $favourite->getPost_id()];
+    $favourite_details= [htmlentities($favourite->getTitle()), $favourite->getPost_id(), $favourite->getPost_date()];
     array_push($member["favourites"], $favourite_details);
 }
 
@@ -24,24 +34,14 @@ foreach($favourites as $favourite){
 $followers= $member_object->getFollowers($pdo);
 $member["followers"]= [];
 foreach($followers as $follower){
-    array_push($member["followers"], $follower);
+    array_push($member["followers"], htmlentities($follower));
 }
 
 //fetching followed members
 $followed= $member_object->getFollowed($pdo);
 $member["followed"]= [];
 foreach($followed as $followed_member){
-    array_push($member["followed"], $followed_member);
-}
-
-//if writer, fetching posts that member has written
-if($member["security_group"]==="writer"){
-    $own_posts= $member_object->getOwnPosts($pdo);
-    $member["own_posts"]= [];
-    foreach($own_posts as $post){
-        $post_details= [$post->getTitle(), $post->getPost_id()];
-        array_push($member["own_posts"], $post_details);
-    }
+    array_push($member["followed"], htmlentities($followed_member));
 }
 
 echo json_encode($member);
