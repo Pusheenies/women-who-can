803e9af8fa8e291248438ff8dd3885b75559bbf4
Natalia Fernández <natalia.fpintos@gmail.com>
2018-05-23 19:25:36 +0100

    Add timed redirection to sign in

--- controllers/sign_up_controller.js
+++ controllers/sign_up_controller.js
@@ -2,15 +2,19 @@ $("#sign-up").submit(function (event) {
     event.preventDefault();
     let data = $("#sign-up").serialize();
     $("#error-msg").html("");
-    console.log(data);
+
     $.post("../../models/sign_up_model.php", data).done(function (response) {
-        console.log(response);
+
         if (response === "Password mismatch") {
             $("#error-msg").html("Your passwords don't match, please try again!");
         } else if (response === "Username exists") {
             $("#error-msg").html("This username already exists, please choose another one!");
         } else if (response === "Sign up successful") {
             $("#error-msg").html("Sign up successful!");
+            let redirect = function () {
+                window.location = "sign_in.php";
+            };
+            setTimeout(redirect, 2000);
         } else {
             $("#error-msg").html("Something went wrong, please try again!");
         }
