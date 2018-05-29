45715714ac97a8af1e0b0bb72e11e1e5911baac2
Laura <35954013+LauraCollard@users.noreply.github.com>
2018-05-29 15:16:29 +0100

    Tweak error msg Update details

--- views/pages/update_details.php
+++ views/pages/update_details.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>WHC - Edit details</title>
+        <title>WWC - Edit details</title>
         <meta charset="UTF-8">    
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
--- views/post_page.php
+++ views/post_page.php
@@ -5,7 +5,7 @@ $_SESSION["post_id"]= $_REQUEST["post"];
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Post</title>
+        <title>WWC</title>
         <meta charset="UTF-8">  
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
